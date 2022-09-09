17ad071a15fb42741ab0711759fc1958376e8eed
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-08 23:05:04 -0700

    Fix errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,14 +12,14 @@ int main(void)
 	char charType;
 	int intType;
 	long int longintType;
-	long long int longlongintType;
+	long long int lliType;
 	float floatType;
 
 	printf("Size of a char: %lu bytes\n", (unsigned long)sizeof(charType));
 	printf("Size of a int: %lu bytes\n", (unsigned long)sizeof(intType));
 	printf("Size of a long int: %lu bytes\n", (unsigned long)sizeof(longintType));
-	printf("Size of a long long int: %lu bytes\n", (unsigned long)sizeof(longlongintType));
+	printf("Size of a long long int: %lu bytes\n", (unsigned long)sizeof(lliType));
 	printf("Size of a float: %lu bytes\n", (unsigned long)sizeof(floatType));
 
-	return(0);
+	return (0);
 }
