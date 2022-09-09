30d01dd617e644deb1f9846106e8e08d5e90c279
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-08 21:05:48 -0700

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	/* This function prints a string */
+	puts("\"Programming is like building a multilingual puzzle");
+	(return 0);
 }
