74847afc02032192a0ef36ce2484e2ea3d8aefa3
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-08 21:45:37 -0700

    Fix errors

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,11 @@
  *
  * Descrition - Prints string
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  **/
 
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
