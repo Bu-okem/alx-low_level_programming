21e4a0223335c455420444239b6d775c7edf62da
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-08 21:41:32 -0700

    Tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#include<stdio.h>                                                                                                                                                                   
-/**                                                                                                                                                                                 
- * main - Entry point                                                                                                                                                               
- *                                                                                                                                                                                  
- * Description: 'the program's description'                                                                                                                                         
- *                                                                                                                                                                                  
- * Return: Always 0 (Success)                                                                                                                                                       
- **/                                                                                                                                                                                 
+include <stdio.h>
+/**
+ * main - Entry piont
+ *
+ * Descrition - Prints string
+ *
+ * Return: Always 0 (Success) 
+ **/
 
-int main(void)                                                                                                                                                                      
-{
-	printf("with proper grammar, but the outcome is a piece of art,");
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}                         
+}
