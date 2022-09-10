db38561537116a94735c48a413efe8a1b0054adb
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-09 19:23:24 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,15 @@ int main(void)
 	for (num = '0'; num <= '9'; ++num)
 	{
 		putchar(num);
-		
+
 		if (num != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	
+	putchar(' ');
 
 	return (0);
 }
