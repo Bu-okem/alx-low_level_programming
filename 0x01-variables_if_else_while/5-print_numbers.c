c0b893f39a35ffe1dbe4b88fd5eb2a7f823ba91e
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-09 18:28:13 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		++i;
+	}
+
+	printf('\n');
+
+	return (0);
+}
