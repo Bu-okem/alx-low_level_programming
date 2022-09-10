b1d24740bd97322da0a5921778dfb58e70edd144
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-09 17:01:03 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stblib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowAlpha;
+
+	for (lowAlpha = 'a', lowAlpha <= 'z', ++lowAlpha)
+		putchar(lowAlpha);
+		putchar("\n")
+	
+	return (0);
+}
