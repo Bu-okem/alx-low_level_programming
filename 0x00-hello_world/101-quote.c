c27b3f654da8884f00053bdec69c44cfce94da36
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-11 00:21:46 -0700

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 /**
  * main - Entry
@@ -8,6 +11,6 @@
 
 int main(void)
 {
-	fwrite(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	write(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 	return (0);
 }
