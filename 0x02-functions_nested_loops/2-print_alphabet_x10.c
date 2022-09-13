1210f063a25ecba94ddb457fcb8e9e591a87e213
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-13 16:12:58 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - function that prints 10 times the alphabet, in lowercase, 
+ * followed by a new line
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (alpha  = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
