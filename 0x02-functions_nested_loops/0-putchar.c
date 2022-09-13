8bcf2bac35b35b5bcea6a751082f31d132b2e0fc
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-13 15:28:19 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar";
+
+	printf(word[]);
+	printf("\n");
+}
