514880fa69132834ccd67171ad6e3f8b3a995b3f
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-13 19:44:52 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * Description - function that prints all natural numbers from n to 98,
+ * followed by a new line
+ * @c: input
+ * Return: Nothing
+ */
+
+void print_to_98(void)
+{
+	if (c <= 98)
+	{
+		for (; c <= 98; c++)
+		{
+			printf("%d", c);
+
+			if (c == 98)
+				continue;
+			printf(', ');
+		}
+
+		printf('\n');
+
+	}
+	else
+	{
+		for (; c <= 98; c--)
+		{
+			printf("%d", c);
+
+			if (c == 98)
+				continue;
+			
+			printf('\n');
+		}
+
+		printf('\n');
+	}
+}
