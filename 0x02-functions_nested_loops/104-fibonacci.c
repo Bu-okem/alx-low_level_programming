1f137253ef2680c90afe368e19915331e4beb1ac
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-13 23:16:15 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 
-	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+	printf("%llu, %llu, ", fibonacci[0], fibonacci[1]);
 
 	for (n = 2; n < 98; n++)
 	{
