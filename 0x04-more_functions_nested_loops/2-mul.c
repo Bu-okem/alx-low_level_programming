e6b4ea474de7bc281a0ba09225abfc69782292ed
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-15 21:28:41 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description - function that multiplies two integers
+ * @a: int input
+ * @b: int input
+ * Return: product of inputs
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
