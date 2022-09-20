858beb4e95f7870da31caa421f25c3fd81c343bf
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-20 07:09:36 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** swap_int - Entry point
+ * Description - function that swaps the values of two integers
+ * @a: int input
+ * @b: int input
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
