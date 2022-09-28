0c437b2f7730c20758e8b564029d1afd0468a157
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-09-28 12:50:30 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry point
+ * Description - function that returns the natural square root of a number
+ * @n: integer input
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(1, n));
+}
+
+/**
+ * _sqrt - Entry point
+ * Description - find square root of input
+ * @prev: previous value
+ * @root: square root value
+ * Return: square root
+ */
+
+int _sqrt(int prev, int root)
+{
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
+}
