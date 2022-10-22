1d539bfdb51e7edb9df8becd4c3fb90227118115
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-10-21 20:13:56 -0700

    Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main_constructor(void) __attribute__((constructor));
+
+/**
+ * main_constructor - Entry point
+ * Description - function that prints a message before main runs
+ * Return: nothing
+ */
+
+void main_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
