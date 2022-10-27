e47ee9e4edf020dd31f4534d0fc5a51297552998
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-10-26 23:15:02 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Entry point
+ * Description - function that frees a listint_t list
+ * @head: pointer to listint_t structure
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	temp = *head;
+	while (*head)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
